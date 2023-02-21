4e798baaf23184c670a41c862aeae4b4f3ac4b97
Júlia Cordeiro <julia.c2001@hotmail.com>
2023-02-21 16:47:03 -0300

    Import csv file on Madaline class

--- adaline/main_view.py
+++ adaline/main_view.py
@@ -14,35 +14,42 @@ def draw_figure(canvas, figure):
 def make_window():
     sg.theme('SystemDefault')
     menu_def = [['&Application', ['E&xit']],
-                ['&Help', ['&About']] ]
+                ['&Help', ['&About']]]
 
     input_grid_namager = [[sg.Text('Input', font='bold 14')],
-                          [sg.Checkbox('', pad=(0,0), key=f'-CB0{i}') for i in range (0,7)],
-                          [sg.Checkbox('', pad=(0,0), key=f'-CB1{i}') for i in range (0,7)],
-                          [sg.Checkbox('', pad=(0,0), key=f'-CB2{i}') for i in range (0,7)],
-                          [sg.Checkbox('', pad=(0,0), key=f'-CB3{i}') for i in range (0,7)],
-                          [sg.Checkbox('', pad=(0,0), key=f'-CB4{i}') for i in range (0,7)],
-                          [sg.Checkbox('', pad=(0,0), key=f'-CB5{i}') for i in range (0,7)],
-                          [sg.Checkbox('', pad=(0,0), key=f'-CB6{i}') for i in range (0,7)],
-                          [sg.Checkbox('', pad=(0,0), key=f'-CB7{i}') for i in range (0,7)],
-                          [sg.Checkbox('', pad=(0,0), key=f'-CB8{i}') for i in range (0,7)],
-                        ]
+                          [sg.Checkbox('', pad=(0, 0), key=f'-CB0{i}') for i in range(0, 7)],
+                          [sg.Checkbox('', pad=(0, 0), key=f'-CB1{i}') for i in range(0, 7)],
+                          [sg.Checkbox('', pad=(0, 0), key=f'-CB2{i}') for i in range(0, 7)],
+                          [sg.Checkbox('', pad=(0, 0), key=f'-CB3{i}') for i in range(0, 7)],
+                          [sg.Checkbox('', pad=(0, 0), key=f'-CB4{i}') for i in range(0, 7)],
+                          [sg.Checkbox('', pad=(0, 0), key=f'-CB5{i}') for i in range(0, 7)],
+                          [sg.Checkbox('', pad=(0, 0), key=f'-CB6{i}') for i in range(0, 7)],
+                          [sg.Checkbox('', pad=(0, 0), key=f'-CB7{i}') for i in range(0, 7)],
+                          [sg.Checkbox('', pad=(0, 0), key=f'-CB8{i}') for i in range(0, 7)],
+                          ]
     input_selectors_manager = [
-                               [sg.Combo(['Val1', 'Val2', 'Val3'], 'Val1', size=10)],
-                               [sg.Button('Classify', size=10)],
-                              ]
+        [sg.Combo(['Val1', 'Val2', 'Val3'], 'Val1', size=10)],
+        [sg.Button('Classify', size=10)],
+    ]
 
-    layout = [[sg.MenubarCustom(menu_def, key='-MENU-')], 
+    layout = [[sg.MenubarCustom(menu_def, key='-MENU-')],
               [sg.Text('Training parameters', font='bold 14')],
-              [sg.Text('Learning Rate', size=10, justification='right', tooltip='Learning rate used in training. Use values between 0.0 and 1.0.'), sg.Input(key='-LEARNING_RATE-')],
-              [sg.Text('Epochs', size=10, justification='right', tooltip='Max number of epochs to be used in training.'), sg.Input(key='-MAX_EPOCHS-')],
-              [sg.Text('Minimum error', size=10, justification='right', tooltip='Error value that stops training once achieved. Use values between 0.0 and 1.0'), sg.Input(key='-MIN_ERROR-')],
+              [sg.Text('Learning Rate', size=10, justification='right',
+                       tooltip='Learning rate used in training. Use values between 0.0 and 1.0.'),
+               sg.Input(key='-LEARNING_RATE-')],
+              [sg.Text('Epochs', size=10, justification='right',
+                       tooltip='Max number of epochs to be used in training.'), sg.Input(key='-MAX_EPOCHS-')],
+              [sg.Text('Minimum error', size=10, justification='right',
+                       tooltip='Error value that stops training once achieved. Use values between 0.0 and 1.0'),
+               sg.Input(key='-MIN_ERROR-')],
               [sg.Button('Train', size=15)],
               [sg.HSeparator()],
-              [sg.Column(input_grid_namager), sg.Column(input_selectors_manager, vertical_alignment='top'), sg.VSeparator(), sg.Canvas(key='-CANVAS-')]
-            ]
+              [sg.Column(input_grid_namager), sg.Column(input_selectors_manager, vertical_alignment='top'),
+               sg.VSeparator(), sg.Canvas(key='-CANVAS-')]
+              ]
     layout[-1].append(sg.Sizegrip())
-    window = sg.Window('Adaline', layout, grab_anywhere=True, resizable=True, margins=(0,0), use_custom_titlebar=True, finalize=True, keep_on_top=True)
+    window = sg.Window('Adaline', layout, grab_anywhere=True, resizable=True, margins=(0, 0), use_custom_titlebar=True,
+                       finalize=True, keep_on_top=True)
 
     # TEST VALUES FOR GRAPHS
     error_graph = graph.draw_graph(error, epoch, 0.987561)
@@ -53,7 +60,8 @@ def make_window():
     window.set_min_size(window.size)
     return window
 
-#TODO handle main_view events (train, classify)
+
+# TODO handle main_view events (train, classify)
 def main():
     window = make_window()
     while True:
@@ -67,7 +75,7 @@ def main():
                      'Enter training parameters on the input fields',
                      'Accepeted parameters: Learning rate, epochs, minimum error',
                      'Press "Train" to train the network, and see it\'s graph on the graph panel',
-                     'Test pre-made or custom inputs using the menu on the left', 
+                     'Test pre-made or custom inputs using the menu on the left',
                      'Authors: C. S. Julia; S. R. Nathan. 2023 - IFTM',
                      keep_on_top=True)
     window.close()
--- adaline/network_elements.py
+++ adaline/network_elements.py
@@ -1,33 +1,38 @@
 import numpy as np
 
+
 class Neuron:
     weights = np.random.uniform(low=-0.5, high=0.5, size=21)
 
     def update_weight(self, index, new_weight):
         self.weights[index] = new_weight
-    
+
     def get_weight(self, index):
         return self.weights[index]
 
     def __str__(self):
         return f'Neuron:[Weights:{self.weights}]'
-    
+
+
 class Madaline:
     neurons = []
     bias = np.random.uniform(low=-0.5, high=0.5, size=21)
     output_layer_pure = np.zeros(21, dtype=float)
     output_layer_liquid = np.zeros(21, dtype=int)
     error_history = []
+    inputs = np.loadtxt(open("docs/x.csv", "rb"), delimiter=",", skiprows=1)
+
     def __init__(self):
         i = 0
         while i < 63:
             self.neurons.append(Neuron())
-    
+
     def generate_output(self, input):
         for output_index in range(21):
             self.output_layer_pure[output_index] = self.bias[output_index]
             for input_index in range(63):
-                self.output_layer_pure[output_index] += input[input_index]*self.neurons[input_index].get_weight(output_index)
+                self.output_layer_pure[output_index] += input[input_index] * self.neurons[input_index].get_weight(
+                    output_index)
             if self.output_layer_pure[output_index] >= 0.0:
                 self.output_layer_liquid[output_index] = 1
             else:
@@ -36,7 +41,7 @@ class Madaline:
     def calculate_error(self, target):
         error = 0.0
         for output_index in range(21):
-            error += 0.5*((target[output_index] - self.output_layer_liquid[output_index])**2.0)
+            error += 0.5 * ((target[output_index] - self.output_layer_liquid[output_index]) ** 2.0)
         return error
 
     def train(self, inputs, targets, learning_rate, max_epoch, minimum_error):
@@ -45,36 +50,22 @@ class Madaline:
         while epoch <= max_epoch and error > minimum_error:
             error = 0.0
             for input_index in range(21):
-                #Process input in the network
+                # Process input in the network
                 self.generate_output(inputs[input_index])
-                #Calculate error
+                # Calculate error
                 error = self.calculate_error(targets[input_index])
-                #Adjust weights
+                # Adjust weights
                 for neuron_index in range(63):
                     for output_index in range(21):
-                        new_weight = self.neurons[neuron_index].get_weight(output_index) + learning_rate * (targets[input_index][output_index] - self.output_layer_liquid[output_index]) * inputs[input_index][neuron_index]
+                        new_weight = self.neurons[neuron_index].get_weight(output_index) + learning_rate * (
+                                    targets[input_index][output_index] - self.output_layer_liquid[output_index]) * \
+                                     inputs[input_index][neuron_index]
                         self.neurons[neuron_index].update_weight(output_index, new_weight)
-            #Save error for history
+            # Save error for history
             self.error_history.append(error)
-            #Increment epochs
+            # Increment epochs
             epoch += 1
             print(f'[LOG] Training... E:{epoch} error:{error * 100}%')
 
     def get_error_history(self):
         return self.error_history
-
-
-    
-
-    
-
-
-
-
-
-
-    
-
-
-
-
