4dcbd3df794cabcafd15f6577318f73ba70534cf
Júlia Cordeiro <julia.c2001@hotmail.com>
2023-02-24 08:19:18 -0300

    Refactor main view file to fix python warnings

--- adaline/main_view.py
+++ adaline/main_view.py
@@ -3,7 +3,13 @@ import numpy as np
 import network_elements as ne
 import graph_generator as graph
 
-COMBO_VALUES = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3', 'D1', 'D2', 'D3', 'E1', 'E2', 'E3', 'J1', 'J2', 'J3', 'K1', 'K2', 'K3']
+COMBO_VALUES = ['A1', 'A2', 'A3',
+                'B1', 'B2', 'B3',
+                'C1', 'C2', 'C3',
+                'D1', 'D2', 'D3',
+                'E1', 'E2', 'E3',
+                'J1', 'J2', 'J3',
+                'K1', 'K2', 'K3']
 CHARACTERS = np.loadtxt(open("docs/x.csv", "rb"), delimiter=",", skiprows=0)
 MADALINE = ne.Madaline()
 CURRENT_COMBO_VALUE = 'A1'
@@ -59,13 +65,15 @@ def make_window():
                        tooltip='Learning rate used in training. Use values between 0.0 and 1.0.'),
                sg.Input(key='-LEARNING_RATE-', default_text=DEFAULT_LEARNING_RATE)],
               [sg.Text('Epochs', size=10, justification='right',
-                       tooltip='Max number of epochs to be used in training.'), sg.Input(key='-MAX_EPOCHS-', default_text=DEFAULT_MAX_EPOCHS)],
+                       tooltip='Max number of epochs to be used in training.'),
+               sg.Input(key='-MAX_EPOCHS-', default_text=DEFAULT_MAX_EPOCHS)],
               [sg.Text('Minimum error', size=10, justification='right',
                        tooltip='Error value that stops training once achieved. Use values between 0.0 and 1.0'),
                sg.Input(key='-MIN_ERROR-', default_text=DEFAULT_MIN_ERROR)],
               [sg.Button('Train', size=15)],
               [sg.HSeparator()],
-              [sg.Column(input_grid_manager, vertical_alignment='top'), sg.Column(input_selectors_manager, vertical_alignment='top'),
+              [sg.Column(input_grid_manager, vertical_alignment='top'),
+               sg.Column(input_selectors_manager, vertical_alignment='top'),
                sg.VSeparator(), sg.Canvas(key='-CANVAS-')]
               ]
     layout[-1].append(sg.Sizegrip())
@@ -80,6 +88,7 @@ def make_window():
 
 # TODO handle classify
 def main():
+    global MADALINE, CURRENT_COMBO_VALUE
     window = make_window()
     while True:
         event, values = window.read(timeout=100)
@@ -90,7 +99,7 @@ def main():
             print('[LOG] Clicked About!')
             sg.popup('Adaline NetworkTrainer',
                      'Enter training parameters on the input fields',
-                     'Accepeted parameters: Learning rate, epochs, minimum error',
+                     'Accepted parameters: Learning rate, epochs, minimum error',
                      'Press "Train" to train the network, and see it\'s graph on the graph panel',
                      'Test pre-made or custom inputs using the menu on the left',
                      'Authors: C. S. Julia; S. R. Nathan. 2023 - IFTM',
@@ -100,7 +109,8 @@ def main():
             learning_rate = float(values['-LEARNING_RATE-'])
             max_epochs = float(values['-MAX_EPOCHS-'])
             min_error = float(values['-MIN_ERROR-'])
-            print(f'[LOG] Calling Train method on network_elements. Values=(learning_rate={learning_rate}, max_epochs={max_epochs}, minimum_error={min_error})')
+            print(f'[LOG] Calling Train method on network_elements. Values=(learning_rate={learning_rate},'
+                  f' max_epochs={max_epochs}, minimum_error={min_error})')
             MADALINE = ne.train(window, learning_rate, max_epochs, min_error)
             window['Classify'].update(disabled=False)
         elif event == 'Classify':
@@ -109,7 +119,7 @@ def main():
             response = MADALINE.classify(COMBO_VALUES.index(CURRENT_COMBO_VALUE))
             print(f'[LOG] Classify response: {response}')
             window['-ANS_TXT-'].update(f'{response}')
-            #TODO Update a text element with the response
+            # TODO Update a text element with the response
         elif event == '-COMBO_BOX-':
             CURRENT_COMBO_VALUE = values['-COMBO_BOX-']
             print(f'[LOG] Combo box changed, value = {CURRENT_COMBO_VALUE}')
